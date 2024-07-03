2552e3140da5894a315b957d3537335abf62574b
GideonBature <infoaboutgideon@gmail.com>
2024-07-03 10:15:29 +0100

    basic game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+
+    let rand_num = rand::thread_rng().gen_range(1..=100);
+
     println!("Welcome to The Number Guessing Game");
 
-    println!("Please enter a guess: ");
+    println!("Please enter a guess between 1 - 100: ");
 
     let mut guess = String::new();
 
@@ -11,5 +16,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("{}", guess);
+    let guess: u32 = match guess.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 5,
+    };
+
+    match guess.cmp(&rand_num) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
