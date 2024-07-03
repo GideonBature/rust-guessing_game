36507c24caf4644cff3593dacd02264f14030b53
GideonBature <infoaboutgideon@gmail.com>
2024-07-03 10:19:54 +0100

    completed the guessing game

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,30 @@ fn main() {
 
     let rand_num = rand::thread_rng().gen_range(1..=100);
 
-    println!("Welcome to The Number Guessing Game");
+    println!("Welcome to The Number Guessing Game\n");
 
-    println!("Please enter a guess between 1 - 100: ");
+    loop {
+        
+        println!("Please enter a guess between 1 - 100: ");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => 5,
-    };
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&rand_num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&rand_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
